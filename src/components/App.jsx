cd04f35b42273c2544fe8cda270c4a23d2bd5faa
Milani Yogeswaran <milaniyoges@gmail.com>
2023-06-20 11:06:38 +0530

    Title chnaged

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,7 +15,7 @@ function App() {
 
       {notes.map((noteItem) => <Note
         key={noteItem.key}
-        title={noteItem.tite}
+        title={noteItem.title}
         content={noteItem.content}
       />
       )}
